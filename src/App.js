c6251f703e6e265255b4f6c02dbd625a4b9822ec
Karthik R <karthikclgid@gmail.com>
2023-06-13 18:16:31 +0530

    add video.mp4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import video from "./assets/video.mp4";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      const mobileWidthThreshold = 1280; // Adjust this value based on your requirements
+      setIsMobile(window.innerWidth < mobileWidthThreshold);
+    };
+
+    // Check initial mobile status
+    checkIsMobile();
+
+    // Add event listener to check mobile status on window resize
+    window.addEventListener('resize', checkIsMobile);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener('resize', checkIsMobile);
+    };
+  }, []);
+
+  if (isMobile) {
+    // Code for mobile devices
+    return (
+      <>
+    <div className="font-bold bg-gradient-to-l from-cyan-400 to-blue-500 min-h-screen">
+      <div className="main flex flex-col lg:flex-row h-full">
+        <div className="left basis-1/2 min-h-screen flex justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
+          Feedback.
+        </div>
+        <div className="right basis-1/2 flex min-h-screen justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
+          Check.
+        </div>
+      </div>
     </div>
-  );
-}
+      </>
+    );
+  } else {
+    // Code for non-mobile (desktop) devices
+    return (
+      <>
+        <div className="relative">
+          <video
+            autoPlay
+            muted
+            loop
+            className="w-full h-auto"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: -1,
+              objectFit: "cover",
+              width: "100vw",
+              height: "100vh",
+            }}
+          >
+            <source src={video} type="video/mp4" />
+          </video>
+
+          <div className="main flex font-bold w-full h-screen ">
+            <div className="left basis-1/2 flex justify-center items-center border-white rounded-[20px] border-[2px] m-20 my-10 backdrop-blur-sm">
+              Feedback.
+            </div>
+            <div className="right basis-1/2 flex justify-center items-center border-white rounded-[20px] border-[2px] m-20 my-10 backdrop-blur-sm">
+              Check.
+            </div>
+          </div>
+
+        </div>
+      </>
+    );
+  }
+};
 
 export default App;
