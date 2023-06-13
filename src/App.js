a099c1c92ab779aa1802da89a13a53d9310c3166
Karthik R <karthikclgid@gmail.com>
2023-06-13 23:00:58 +0530

    first form

--- src/App.js
+++ src/App.js
@@ -26,16 +26,16 @@ const App = () => {
     // Code for mobile devices
     return (
       <>
-    <div className="font-bold bg-gradient-to-l from-cyan-400 to-blue-500 min-h-screen">
-      <div className="main flex flex-col lg:flex-row h-full">
-        <div className="left basis-1/2 min-h-screen flex justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
-          Feedback.
+        <div className="font-bold bg-gradient-to-l from-cyan-400 to-blue-500 min-h-screen">
+          <div className="main flex flex-col lg:flex-row h-full">
+            <div className="left basis-1/2 min-h-screen flex justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
+              Feedback.
+            </div>
+            <div className="right basis-1/2 flex min-h-screen justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
+              Check.
+            </div>
+          </div>
         </div>
-        <div className="right basis-1/2 flex min-h-screen justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
-          Check.
-        </div>
-      </div>
-    </div>
       </>
     );
   } else {
@@ -61,10 +61,28 @@ const App = () => {
             <source src={video} type="video/mp4" />
           </video>
 
-          <div className="main flex font-bold w-full h-screen ">
-            <div className="left basis-1/2 flex justify-center items-center border-white rounded-[20px] border-[2px] m-20 my-10 backdrop-blur-sm">
-              Feedback.
+          <div className="main flex w-full h-screen font-black ">
+            <div className="left flex-col basis-1/2 justify-center items-center border-white rounded-[20px] border-[2px] m-20 my-10 backdrop-blur-sm">
+
+              <div className='left_heading text-4xl text-white pt-10 pl-10'>
+                Feedback.
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  autoComplete='off'
+                  className="m-6 placeholder:text-gray-600 text-white rounded-lg outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
+
+
+
+
             </div>
+
             <div className="right basis-1/2 flex justify-center items-center border-white rounded-[20px] border-[2px] m-20 my-10 backdrop-blur-sm">
               Check.
             </div>
