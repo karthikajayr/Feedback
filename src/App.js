2ba545a979c5edf7835073b5a5729ca26f778564
Karthik R <karthikclgid@gmail.com>
2023-10-13 14:23:16 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -36,14 +36,156 @@ const App = () => {
       <>
         <div className="font-bold bg-gradient-to-l from-cyan-400 to-blue-500 min-h-screen">
           <div className="main flex flex-col lg:flex-row h-full">
-            <div className="left basis-1/2 min-h-screen flex justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
-              Feedback.
+            <div className="left flex-col basis-1/2 justify-center items-center border-white rounded-[20px] border-[2px] m-5 my-10 backdrop-blur-sm">
+
+              <div className='left_heading text-4xl text-white pt-10 pl-10 pb-2'>
+                Feedback.
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name*"
+                  autoComplete='off'
+                  required
+                  className="m-6 mx-10 placeholder:text-white text-white  outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type='email'
+                  name="email"
+                  placeholder="E-Mail*"
+                  autoComplete='off'
+                  required
+                  className="m-6 mx-10 placeholder:text-white text-white  outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type='text'
+                  name="occupation"
+                  placeholder="Occupation*"
+                  autoComplete='off'
+                  required
+                  className="m-6 mx-10 placeholder:text-white text-white outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type='text'
+                  name="organization"
+                  placeholder="Organization"
+                  autoComplete='off'
+                  className="m-6 mx-10 placeholder:text-white text-white outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type='text'
+                  name="githubusername"
+                  placeholder="Github Username"
+                  autoComplete='off'
+                  className="m-6 mx-10 placeholder:text-white text-white outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type='text'
+                  name="linkedinprofile"
+                  placeholder="LinkedIn Profile"
+                  autoComplete='off'
+                  className="m-6 mx-10 placeholder:text-white text-white outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
             </div>
-            <div className="right basis-1/2 flex min-h-screen justify-center items-center border-white rounded-[20px] border-[2px] m-5 backdrop-blur-sm">
-              Check.
+
+
+            <div className="right basis-1/2 flex-col justify-center items-center border-white rounded-[20px] border-[2px] m-5 my-10 backdrop-blur-sm">
+
+              <div className='left_heading text-2xl text-white pt-12 pl-10 pb-2'>
+                Project Details.
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type='text'
+                  name="projectname"
+                  placeholder="Project Name*"
+                  autoComplete='off'
+                  required
+                  className="m-6 mx-10 placeholder:text-white text-white outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div>
+
+              <div className='flex mx-10 justify-between items-center m-5'>
+                <div className='flex  text-gray-300'>
+                  Browse Projects in My Github Repositories
+                </div>
+
+                <div className="flex ">
+                  <button className="flex justify-between items-center gap-5 border border-gray-500 hover:border-white text-gray-500 hover:text-white font-bold px-2 rounded ">
+                    <img src={github} alt="Logo" className="logo h-[50px] w-[50px] " />
+                    <div className=' hidden gitname:block'>
+                      Github
+                    </div>
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex flex-col">
+                <textarea
+                  name="comments"
+                  id="comments"
+                  rows={3}
+                  placeholder="Comments*"
+                  autoComplete='off'
+                  required
+                  className="m-6 mx-10 placeholder:text-white text-white outline-none border-b-2 border-white p-2 bg-transparent"
+                />
+              </div >
+
+
+              <div className=' text-white m-3 mx-10 p-2'>
+                Overall Ratings
+              </div>
+              <div className='flex justify-center items-center m-2'>
+                <div>
+                  <StarRating rating={rating} onRatingChange={handleRatingChange} />
+                  {/* <p className=' text-white'>Selected rating: {rating}</p> */}
+                </div>
+              </div>
+
+              <div className='flex justify-between mx-10 pt-10 p-5 m-3 '>
+                <button
+                  type='submit'
+                  className="flex justify-between items-center gap-5 border border-white hover:border-green-500 text-white hover:text-green-500 font-bold py-2 px-4 rounded ">
+                  Submit
+                </button>
+                <button
+                  type='clear'
+                  className="flex justify-between items-center gap-5 border border-white hover:border-red-500 text-white hover:text-red-500 font-bold py-2 px-4 rounded ">
+                  Clear
+                </button>
+              </div>
+
             </div>
           </div>
+          <div className="text-center w-full p-1 text-sm md:text-base">
+            <hr className="w-full h-0.5 mx-auto my-4 bg-white border-0 rounded md:my-1.5"></hr>
+            <p>
+              Copyrights &copy; 2023 Karthik R | Voidex | All Rights Reserved
+            </p>
+          </div>
         </div>
+
+
       </>
     );
   } else {
@@ -193,19 +335,19 @@ const App = () => {
               <div className='flex justify-center items-center m-2'>
                 <div>
                   <StarRating rating={rating} onRatingChange={handleRatingChange} />
-                  <p className=' text-white'>Selected rating: {rating}</p>
+                  {/* <p className=' text-white'>Selected rating: {rating}</p> */}
                 </div>
               </div>
 
-              <div className='flex justify-between mx-10 p-5 m-3 '>
-                <button 
-                type='submit'
-                className="flex justify-between items-center gap-5 border border-gray-500 hover:border-green-500 text-gray-500 hover:text-green-500 font-bold py-2 px-4 rounded ">
+              <div className='flex justify-between mx-10 pt-10 p-5 m-3 '>
+                <button
+                  type='submit'
+                  className="flex justify-between items-center gap-5 border border-gray-500 hover:border-green-500 text-gray-500 hover:text-green-500 font-bold py-2 px-4 rounded ">
                   Submit
                 </button>
-                <button 
-                type='clear'
-                className="flex justify-between items-center gap-5 border border-gray-500 hover:border-red-500 text-gray-500 hover:text-red-500 font-bold py-2 px-4 rounded ">
+                <button
+                  type='clear'
+                  className="flex justify-between items-center gap-5 border border-gray-500 hover:border-red-500 text-gray-500 hover:text-red-500 font-bold py-2 px-4 rounded ">
                   Clear
                 </button>
               </div>
